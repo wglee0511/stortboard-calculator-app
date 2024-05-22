0f829fd16521ee7e0586dc9a48bdbec2356b9bb5
wglee0511 <ranazang@naver.com>
2024-05-22 23:19:39 +0900

    feature: calculator

--- StoryboardCalculatorApp/ViewController.swift
+++ StoryboardCalculatorApp/ViewController.swift
@@ -8,7 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstCalcValue: UITextField!
+    @IBOutlet weak var secondCalcValue: UITextField!
+    @IBOutlet weak var resultCalcValue: UITextField!
+    @IBOutlet weak var operatorCalcValue: UIButton!
+    
+    let operatorValues: [String] = ["+", "-", "*", "/"]
+    
+    @IBAction func onPressOperator(_ sender: Any) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        for operatorValue in operatorValues {
+            setCalculateAction(title: operatorValue, actionSheet: actionSheet, buttonSheet: self.operatorCalcValue)
+        }
+        
+        present(actionSheet, animated: true)
+    }
+    
+    
+    @IBAction func onPressCalculator(_ sender: Any) {
+        let firstValue = Int(firstCalcValue.text ?? "0") ?? 0
+        let secondValue = Int(secondCalcValue.text ?? "0") ?? 0
+        let operatorValue = operatorCalcValue.title(for: .normal) ?? "+"
+        
+        let result = getCalculateResult(operatorValue: operatorValue, firstValue: firstValue, secondValue: secondValue)
+        
+        resultCalcValue.text = "\(result)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ lib/calc.swift
@@ -0,0 +1,33 @@
+//
+//  calc.swift
+//  StoryboardCalculatorApp
+//
+//  Created by racoon on 5/22/24.
+//
+
+import Foundation
+import UIKit
+
+func setCalculateAction(title: String, actionSheet: UIAlertController, buttonSheet: UIButton) {
+    let action = UIAlertAction(title: title, style: .default) { _ in
+        
+        buttonSheet.setTitle(title, for: .normal)
+    }
+    actionSheet.addAction(action)
+}
+
+
+func getCalculateResult(operatorValue: String, firstValue: Int, secondValue: Int) -> String {
+    switch operatorValue {
+    case "+":
+        return "\(firstValue + secondValue)"
+    case "-":
+        return "\(firstValue - secondValue)"
+    case "*":
+        return "\(firstValue * secondValue)"
+    case "/":
+        return "\(firstValue / secondValue)"
+    default:
+        return "0"
+    }
+}
